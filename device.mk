5188f6f0997069b4542725d6a9f19ca685d5ca14
kyasu <kyasu.dev@gmail.com>
2020-01-06 16:12:16 +0900

    ks01ltexx: Convert lights HAL into a binderized HAL

--- device.mk
+++ device.mk
@@ -74,8 +74,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    lights.MSM8974
+    android.hardware.light@2.0-service.samsung
 
 # Media
 PRODUCT_COPY_FILES += \
